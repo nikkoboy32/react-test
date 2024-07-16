54649a727f8f69a1f771ca69cc3c3727cb3268f3
Nikko <117736564+nikkoboy32@users.noreply.github.com>
2024-07-16 21:02:53 +0800

    fixed image

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,10 +5,10 @@ export default function Footer() {
         <footer>
             <div className="social_media_icons_con">
                 <div className="social_media_icons_flex">
-                <a href="javacsript:;"><figure><img src="../public/images/icons/Twitter-Icon.png" alt="twitter icon"/></figure></a>
-                <a href="javacsript:;"><figure><img src="../public/images/icons/Facebook-Icon.png" alt="facebook icon"/></figure></a>
-                <a href="javacsript:;"><figure><img src="../public/images/icons/Instagram-Icon.png" alt="instagram icon"/></figure></a>
-                <a href="javacsript:;"><figure><img src="../public/images/icons/GitHub-Icon.png" alt="github icon"/></figure></a>
+                <a href="javacsript:;"><figure><img src="/public/images/icons/Twitter-Icon.png" alt="twitter icon"/></figure></a>
+                <a href="javacsript:;"><figure><img src="/public/images/icons/Facebook-Icon.png" alt="facebook icon"/></figure></a>
+                <a href="javacsript:;"><figure><img src="/public/images/icons/Instagram-Icon.png" alt="instagram icon"/></figure></a>
+                <a href="javacsript:;"><figure><img src="/public/images/icons/GitHub-Icon.png" alt="github icon"/></figure></a>
                 </div>
             </div>
         </footer>
